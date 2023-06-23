411096ed086b997ad2a7601b0d184e9fb28e6634
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2023-06-23 15:04:59 -0300

    cores, fontes, espaçamentos inseridos no arquivo de configuração

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-gray": "#75767A",
+        "dark-gray": "#2E3038",
+      },
+      fontFamily: {
+        prata: ["Prata", "serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        36: "36px",
+        56: "56px",
+      },
+      lineHeight: {
+        12: "50px",
+        27: "27px",
+        73: "73px",
+      },
+    },
   },
   plugins: [],
 };
